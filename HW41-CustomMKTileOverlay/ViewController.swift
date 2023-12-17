5f16ec9aa08672b691f6d900ce5752e21a8b0d91
Daweihao84 <dwsamurai84@gmail.com>
2023-12-17 17:05:07 +0800

    Initial Commit

---
+++ HW41-CustomMKTileOverlay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW41-CustomMKTileOverlay
+//
+//  Created by Dawei Hao on 2023/12/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HW41-CustomMKTileOverlayUITests/HW41_CustomMKTileOverlayUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  HW41_CustomMKTileOverlayUITestsLaunchTests.swift
+//  HW41-CustomMKTileOverlayUITests
+//
+//  Created by Dawei Hao on 2023/12/17.
+//
+
+import XCTest
+
+final class HW41_CustomMKTileOverlayUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
